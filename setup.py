2aeb5bd9733ecef2ce9c5c9a53f6fa1106eb1dc3
maximelqt <maximeliquet@free.fr>
2019-08-11 00:02:37 +0200

    Much better tox.ini

--- setup.py
+++ setup.py
@@ -7,6 +7,25 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+AUTOMATION_REQUIRE = ["tox>=3.12.1"]
+LINTERS_REQUIRE = ["flake8>=3.7.8"]
+FORMATTER_REQUIRE = ["black>=19.3b0"]
+NOTEBOOK_REQUIRE = ["jupyterlab>=1.0.4"]
+TEST_REQUIRE = ["pytest>=5.0.1", "pytest-cov>=2.7.1"]
+TEST_NOTEBOOKS_REQUIRE = ["pytest>=5.0.1", "nbval>=0.1"]
+DOCS_REQUIRE = ["sphinx>=2.1.2", "sphinx-rtd-theme>=0.4.3"]
+DEV_REQUIRE = list(
+    set(
+        AUTOMATION_REQUIRE
+        + LINTERS_REQUIRE
+        + FORMATTER_REQUIRE
+        + NOTEBOOK_REQUIRE
+        + TEST_REQUIRE
+        + DOCS_REQUIRE
+        + TEST_NOTEBOOKS_REQUIRE
+    )
+)
+
 # This call to setup() does all the work
 setup(
     name="project",
@@ -27,5 +46,13 @@ setup(
     packages=["project"],
     include_package_data=True,
     install_requires=[],
+    extras_require={
+        "dev": DEV_REQUIRE,
+        "test": TEST_REQUIRE,
+        "test_notebooks": TEST_NOTEBOOKS_REQUIRE,
+        "formatter": FORMATTER_REQUIRE,
+        "linters": LINTERS_REQUIRE,
+        "docs": DOCS_REQUIRE,
+    },
     project_urls={"Documentation": "https://xyxyxy.readthedocs.io/en/latest/"},
 )
